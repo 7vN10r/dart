8e82f2aaf8790c3bfff13b1b6956d97cbf11aae1
Diar J R <diar.student@gmail.com>
2024-05-07 15:59:25 +0700

    Update: using Switch Expression

--- bin/switch.dart
+++ bin/switch.dart
@@ -5,51 +5,17 @@ void main(List<String> args) {
 
   int number = int.tryParse(input) ?? 0;
 
-  switch (number) {
-    case 1:
-      print("one");
-      break;
-    case 2:
-      print("two");
-      break;
-    case 3:
-      print("three");
-      break;
-    case 4:
-      print("four");
-      break;
-    case 5:
-      print("five");
-      break;
-    case 6:
-      print("six");
-      break;
-    case 7:
-      print("seven");
-      break;
-    case 8:
-      print("eight");
-      break;
-    case 9:
-      print("nine");
-      break;
-    case 10:
-      print("ten");
-      break;
-    case 11:
-      print("eleven");
-      break;
-    case 12:
-      print("twelve");
-      break;
-    case 13:
-      print("thirteen");
-      break;
-    case 14:
-      print("fourteen");
-      break;
-    default:
-      print("other");
-      break;
-  }
+  String output;
+
+  output = switch (number) {
+    1 => 'one',
+    2 => 'two',
+    3 => 'three',
+    4 => 'four',
+    5 => 'five',
+    6 => 'six',
+    _ => 'other number' // default case
+  };
+
+  print(output);
 }
